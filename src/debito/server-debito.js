05d9e238ef7c5eb0c599c24cd3418f241d5a5b81
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-02-04 18:24:32 -0200

    Criação de um arquivo 'test.json' na pasta config do projeto.

--- src/debito/server-debito.js
+++ src/debito/server-debito.js
@@ -16,3 +16,14 @@ var port = 3000;
 
 var post = require('./api/v1/debito');
 var config = require('config'); // aqui estaremos carregando a localização da base de dados através dos arquivos JSON.
+
+//Opção das base de dados:
+var options = {
+  server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+};
+
+//Conexão com a base de dados:
+mongoose.connect(config.DBHost, options);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro ao conectar com a Base de Dados....: '));
