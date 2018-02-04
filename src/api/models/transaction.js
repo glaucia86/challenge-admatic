f3d9cb94ac98fbcb5227ed4eb9420663361c0d9b
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-02-04 17:29:52 -0200

    Adicionando mais arquivos no projeto.

---
+++ src/api/models/transaction.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * Arquivo: api/models/transaction.js
+ * Author: Glaucia Lemos
+ * Description: Arquivo responsável pelo modelo: 'Transaction' para realizar a conexão com a base
+ *  de dados via Moongose.
+ * Data: 04/02/2018
+ *
+ */
