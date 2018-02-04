d748a3a57446fe498fc26f0c2de0939f0d5ad7f2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-02-04 17:38:53 -0200

    Reestruturação do projeto.

--- src/api/v1/debit.js
+++
@@ -1,8 +0,0 @@
-/*
- *
- * Arquivo: api/v1/debit.js
- * Author: Glaucia Lemos
- * Description: Arquivo responsável pelas rotas das APIS: GET, PUT, DELETE & POST
- * Data: 04/02/2018
- * 
- */
--- src/helper/random-error-in-ten.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-module.exports = (chance, error) => {
-  if (Math.random() * (10 - 0) + 0 > chance) throw error;
-};
